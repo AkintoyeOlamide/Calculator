1fc77a10cd3b18e1f3c06ade8bb16a8245f588b8
olamidelanky <akintoyeolamide95@gmail.com>
2021-07-14 12:17:14 +0100

    finished milestone4

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
+
 import '../App.css';
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 const App = () => (
   <>
